82335f73e08223d17bad74635cd9cb85aad42772
Vladimir Ković <vlada.kovic@gmail.com>
2021-02-17 14:36:16 +0100

    Basics

--- database/migrations/2021_02_17_132220_create_traders_table.php
+++ database/migrations/2021_02_17_132220_create_traders_table.php
@@ -15,7 +15,28 @@ class CreateTradersTable extends Migration
     {
         Schema::create('traders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table->string('uid', 32);
+            $table->string('name');
+            $table->boolean('sharing');
+            $table->string('twitter');
+
+            $table->unsignedSmallInteger('ranking');
+            $table->unsignedSmallInteger('ranking_day');
+            $table->unsignedSmallInteger('ranking_week');
+            $table->unsignedSmallInteger('ranking_month');
+
+            $table->unsignedSmallInteger('roi');
+            $table->unsignedSmallInteger('roi_day');
+            $table->unsignedSmallInteger('roi_week');
+            $table->unsignedSmallInteger('roi_month');
+
+            $table->unsignedSmallInteger('pnl');
+            $table->unsignedSmallInteger('pnl_day');
+            $table->unsignedSmallInteger('pnl_week');
+            $table->unsignedSmallInteger('pnl_month');
+
+            $table->dateTime('unshared_at');
         });
     }
 
--- database/migrations/2021_02_17_132233_create_positions_table.php
+++ database/migrations/2021_02_17_132233_create_positions_table.php
@@ -15,6 +15,22 @@ class CreatePositionsTable extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('trader_id');
+
+            $table->string('symbol', 32);
+            $table->double('size');
+            $table->double('invested');
+            $table->unsignedTinyInteger('leverage');
+            $table->double('cost');
+            $table->double('entry_price');
+            $table->double('current_price');
+            $table->double('pnl');
+            $table->double('roe');
+
+            $table->dateTime('provider_updated_at');
+            $table->dateTime('opened_at');
+            $table->dateTime('closed_at');
+
             $table->timestamps();
         });
     }
