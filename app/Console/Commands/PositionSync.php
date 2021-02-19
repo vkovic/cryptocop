045d025dd3c65b9a75ebc7da95b91688e819515f
Vladimir Ković <vlada.kovic@gmail.com>
2021-02-19 15:16:55 +0100

    Positions sync

--- app/Console/Commands/PositionSync.php
+++ app/Console/Commands/PositionSync.php
@@ -48,14 +48,20 @@ class PositionSync extends Command
             $bodyObj = json_decode($rawBody);
             $data = $bodyObj->data;
 
+            // Ids of positions which are obtained from the API.
+            // Those are currently present positions for user.
+            // Closed positions are not included in the response.
+            $presentTradeIds = [];
+
             foreach ($data->otherPositionRetList as $respPosition) {
-                // Position ids of just updated and created positions
-                $updatedAndCreatedIds = [];
+                $openedAt = Carbon::createFromTimestampMs($respPosition->updateTimeStamp);
 
                 $position = Position::where([
+                    'trader_id' => $trader->id,
                     'symbol' => $respPosition->symbol,
                     'entry_price' => $respPosition->entryPrice,
                     'size' => $respPosition->amount,
+                    'opened_at' => $openedAt,
                     'closed_at' => null
                 ])->first();
 
@@ -75,25 +81,23 @@ class PositionSync extends Command
                     $position->size = $respPosition->amount;
                     $position->entry_price = $respPosition->entryPrice;
                     $position->mark_price = $respPosition->markPrice;
-
                     $position->pnl = $respPosition->pnl;
                     $position->roe = $respPosition->roe * 100;
-
-                    $position->opened_at = Carbon::createFromTimestampMs($respPosition->updateTimeStamp);
+                    $position->opened_at = $openedAt;
                 }
 
                 $position->save();
 
-                $updatedAndCreatedIds[] = $position->refresh()->id;
-
-                // Close positions for current trader,
-                // which do not exist in the list obtained from the API
-                Position::whereNotIn('id', $updatedAndCreatedIds)
-                    ->where(['closed_at' => null, 'trader_id' => $trader->id])
-                    ->update(['closed_at' => Carbon::now()]);
+                $presentTradeIds[] = $position->refresh()->id;
 
                 usleep(100000); // 0.1 sec
             }
+
+            // Close positions for current trader,
+            // which do not exist in the list obtained from the API
+            Position::whereNotIn('id', $presentTradeIds)
+                ->where('trader_id', $trader->id)
+                ->update(['closed_at' => Carbon::now()]);
         }
 
         return 0;
